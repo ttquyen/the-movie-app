cb7b55b88235c899c2f790ae306917d06bb5ede4
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-09-21 18:14:31 +0700

    👌 update logo size in Login Page

--- src/layouts/BlankLayout.js
+++ src/layouts/BlankLayout.js
@@ -14,7 +14,7 @@ function BlankLayout() {
   return (
     <Stack minHeight="100vh" justifyContent="center" alignItems="center">
       <HeaderStyle>
-        <Logo sx={{ width: 70, height: 70 }} />
+        <Logo sx={{ width: 170, height: 170 }} />
       </HeaderStyle>
       <Outlet />
     </Stack>
