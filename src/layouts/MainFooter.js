48764247cf4ac015cfae4a64c952152d93ca7084
Thuy-Quyen Truong <thuyquyentruong543@gmail.com>
2023-09-21 18:28:47 +0700

    👌 remove footer

--- src/layouts/MainFooter.js
+++ src/layouts/MainFooter.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { Link, Typography } from "@mui/material";
+// import { Link, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 
 function MainFooter() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" p={1}>
-      {"Copyright © "}
-      <Link color="inherit" href="https://www.coderschool.vn">
-        CoderSchool
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+    // <Typography variant="body2" color="text.secondary" align="center" p={1}>
+    //   {"Copyright © "}
+    //   <Link color="inherit" href="https://www.coderschool.vn">
+    //     CoderSchool
+    //   </Link>{" "}
+    //   {new Date().getFullYear()}
+    //   {"."}
+    // </Typography>
+    <Box sx={{ height: "20px" }}></Box>
   );
 }
 
